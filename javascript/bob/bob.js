97c958a9a76acdd1a04e3684b12f6911ac85fd8a
rchavarria <txingo@gmail.com>
2015-07-12 13:29:38 +0200

    Solved, there are only some refactorings left

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,7 +1,33 @@
 (function () {
+    function isShouting(saying) {
+        return saying === saying.toUpperCase() && saying.match(/[a-zA-Z]/);
+    }
+
+    function isAsking(saying) {
+        return saying.indexOf('?') === saying.length - 1;
+    }
+
+    function isSayingNothing(saying) {
+        return saying.length === 0;
+    }
+
     var Bob = function() {};
 
     Bob.prototype.hey = function(input) {
+        input = input.trim();
+
+        if (isSayingNothing(input)) {
+            return 'Fine. Be that way!';
+        }
+
+        if (isShouting(input)) {
+            return 'Whoa, chill out!';
+        }
+
+        if (isAsking(input)) {
+            return 'Sure.';
+        }
+
         return 'Whatever.';
     };
 
