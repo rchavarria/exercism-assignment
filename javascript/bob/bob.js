4afc72bd43978f5272c45d38ab9c5a1a71a57be5
rchavarria <txingo@gmail.com>
2015-07-12 02:20:45 +0200

    Add some ideas to iterate over this exercise again

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -14,6 +14,16 @@ function onlyContainsUppercase(string) {
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
+    // TODO Mirando otras soluciones por ahí, solo se necesitan 4
+    // returns. El que tiene truco es el de chillar
+    // phrase === phrase.toUpperCase() && phrase.match(/[a-zA-Z]/)
+    // es decir, que es igual que el upper case y estamos seguros de que
+    // tiene letras.
+    // Varias ideas:
+    // 1. cada if tiene un método
+    // 2. las respuestas están en un objeto
+    // 3. cada método devuelve la respuesta o null
+    // 4. hay un bucle para evitar tener tantos ifs
     if (!input.trim()) {
         return 'Fine. Be that way!';
     }
