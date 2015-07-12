c761f79779dd5407b0bcd60d8478c5eeddc7e12d
rchavarria <txingo@gmail.com>
2015-07-12 13:20:02 +0200

    Start solving the exercise from the beginning

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,50 +1,10 @@
-function endsWith(string, ending) {
-    var strRegExp = ending + '$';
-    return string.match(new RegExp(strRegExp));
-}
+(function () {
+    var Bob = function() {};
 
-function containsLowercase(string) {
-    return string.match(/[a-z]/);
-}
-
-function onlyContainsUppercase(string) {
-    return string.match(/^[A-Z\s\?]+$/);
-}
-
-var Bob = function() {};
-
-Bob.prototype.hey = function(input) {
-    // TODO Mirando otras soluciones por ahí, solo se necesitan 4
-    // returns. El que tiene truco es el de chillar
-    // phrase === phrase.toUpperCase() && phrase.match(/[a-zA-Z]/)
-    // es decir, que es igual que el upper case y estamos seguros de que
-    // tiene letras.
-    // Varias ideas:
-    // 1. cada if tiene un método
-    // 2. las respuestas están en un objeto
-    // 3. cada método devuelve la respuesta o null
-    // 4. hay un bucle para evitar tener tantos ifs
-    if (!input.trim()) {
-        return 'Fine. Be that way!';
-    }
-
-    if (onlyContainsUppercase(input)) {
-        return 'Whoa, chill out!';
-    }
-
-    if (endsWith(input, '\\?')) {
-        return 'Sure.';
-    }
-
-    if (containsLowercase(input)) {
+    Bob.prototype.hey = function(input) {
         return 'Whatever.';
-    }
+    };
 
-    if (endsWith(input, '!')) {
-        return 'Whoa, chill out!';
-    }
+    module.exports = Bob;
+})();
 
-    return 'Whatever.';
-};
-
-module.exports = Bob;
