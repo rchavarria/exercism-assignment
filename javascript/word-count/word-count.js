d5e345fd1b28aaa2eedb70a12ed5c51cc9f0a845
rchavarria <txingo@gmail.com>
2015-07-23 22:41:59 +0200

    Use Array.prototype.reduce() instead of Array.prototype.forEach()

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,11 +1,3 @@
-/*
- * Some ideas to improve this iteration:
- * 1. use `Array.prototype.reduce()` 
- * 2. use `/\S+/g`
- * 3. use `match()` instead of `split()`
- * 4. use `Number(result[word])` to check if the property exists and
- * it is a number
- */
 function replaceToSpace(phrase) {
   return phrase.replace(/[\t\n\s]+/, ' ').trim();
 }
@@ -14,19 +6,18 @@ function isCountedWord(candidate) {
   return candidate && typeof candidate !== 'function';
 }
 
-function words(phrase) {
-  var result = {},
-    candidates = replaceToSpace(phrase).split(' ');
-
-  candidates.forEach(function (word) {
-    var count = 0;
-    if (isCountedWord(result[word])) {
-      count = result[word];
-    }
-    result[word] = count + 1;
-  });
-
+function countWords(result, word) {
+  var count = 0;
+  if (isCountedWord(result[word])) {
+   count = result[word];
+  }
+  result[word] = count + 1;
   return result;
+}
+
+function words(phrase) {
+  var candidates = replaceToSpace(phrase).split(' ');
+  return candidates.reduce(countWords, {});
 }
 
 module.exports = words;
