645bb83849cf5021932f6d156fec6753b46a314f
Rubén Chavarría <txingo@gmail.com>
2015-07-24 13:46:57 +0200

    Refactor: simplify by using string[i] instead of Array[i] syntax

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,16 +1,14 @@
 function compute(strand1, strand2) {
-  var i,
-    len = strand1.length,
-    distance = 0,
-    nucleotides1 = strand1.split(''),
-    nucleotides2 = strand2.split('');
-  
   if (strand1.length !== strand2.length) {
     throw new Error('DNA strands must be of equal length.');
   }
 
+  var i,
+    len = strand1.length,
+    distance = 0;
+  
   for(i = 0; i < len; i++) {
-    distance += (nucleotides1[i] === nucleotides2[i]) ? 0 : 1;
+    distance += (strand1[i] === strand2[i]) ? 0 : 1;
   }
 
   return distance;
