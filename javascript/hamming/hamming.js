f1dffd047dbdd7494f64bfc49487f64d6ee3d87a
Rubén Chavarría <txingo@gmail.com>
2015-07-24 13:40:44 +0200

    Make *small hamming distance* test pass

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,8 +1,15 @@
 function compute(strand1, strand2) {
-  if (strand1 === strand2) {
-    return 0;
+  var i,
+    len = strand1.length,
+    distance = 0,
+    nucleotides1 = strand1.split(''),
+    nucleotides2 = strand2.split('');
+
+  for(i = 0; i < len; i++) {
+    distance += (nucleotides1[i] === nucleotides2[i]) ? 0 : 1;
   }
-  return strand1.length;
+
+  return distance;
 }
 
 module.exports = { compute: compute };
