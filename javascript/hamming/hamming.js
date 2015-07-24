95403d64943e21f4b2899ff3de9feb6425862bb8
Rubén Chavarría <txingo@gmail.com>
2015-07-24 13:43:49 +0200

    Make *throws error when strands has not the same length* test pass

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -4,6 +4,10 @@ function compute(strand1, strand2) {
     distance = 0,
     nucleotides1 = strand1.split(''),
     nucleotides2 = strand2.split('');
+  
+  if (strand1.length !== strand2.length) {
+    throw new Error('DNA strands must be of equal length.');
+  }
 
   for(i = 0; i < len; i++) {
     distance += (nucleotides1[i] === nucleotides2[i]) ? 0 : 1;
