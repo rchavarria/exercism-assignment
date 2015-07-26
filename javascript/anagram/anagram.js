a967dc88b4b2b647c72d9972978391756ea28075
Rubén Chavarría <txingo@gmail.com>
2015-07-26 13:16:36 +0200

    Make *detects simple anagram* test pass

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,9 +1,21 @@
+function areAnagram(anagram, candidate) {
+  var sortedAnagram = anagram.split('').sort().join(''),
+    sortedCandidate = candidate.split('').sort().join('');
+
+  return sortedAnagram === sortedCandidate;
+}
+
 function Subject(anagram) {
   this.anagram = anagram;
 }
 
 Subject.prototype.matches = function (candidates) {
-  return [];
+  var self = this,
+    matches = candidates.filter(function (candidate) {
+      return areAnagram(self.anagram, candidate);
+    });
+
+  return matches;
 }
 
 module.exports = function (anagram) {
