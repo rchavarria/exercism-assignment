003a5185bfaabebec46527225d3c1720d5e03872
Rubén Chavarría <txingo@gmail.com>
2015-07-26 13:32:54 +0200

    Make *matches() accepts string arguments* test pass

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -7,12 +7,31 @@ function areAnagram(anagram, candidate) {
     sortStringChars(anagram) === sortStringChars(candidate);
 }
 
+function buildCandidatesAsArray(candidatesAsArgs) {
+  var i,
+    len = candidatesAsArgs.length,
+    candidates = [];
+
+  for(i = 0; i < len; i++) {
+    candidates.push(candidatesAsArgs[i]);
+  }
+
+  return candidates;
+}
+
 function Subject(anagram) {
   this.anagram = anagram;
 }
 
 Subject.prototype.matches = function (candidates) {
-  var matches = candidates.filter(function (candidate) {
+  var matches,
+    candidatesAsArray = candidates;
+
+  if (typeof candidates === 'string') {
+    candidatesAsArray = buildCandidatesAsArray(arguments);
+  }
+
+  matches = candidatesAsArray.filter(function (candidate) {
     return areAnagram(this.anagram, candidate);
   }, this);
 
