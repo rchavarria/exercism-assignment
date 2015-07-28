5f45a52fbce18280530140697dbad64e390a6f05
Rubén Chavarría <txingo@gmail.com>
2015-07-28 08:28:02 +0200

    Use `Array.prototype.slice` to convert `arguments` into a usable `Array`
    Now, the code is much simpler and more compact

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -2,16 +2,6 @@ function sortStringChars(str) {
   return str.split('').sort().join('');
 }
 
-function buildCandidatesAsArray(candidatesAsArgs) {
-  var i, len, candidates = [];
-
-  for(i = 0, len = candidatesAsArgs.length; i < len; i++) {
-    candidates.push(candidatesAsArgs[i]);
-  }
-
-  return candidates;
-}
-
 function Subject(anagram) {
   this.anagram = anagram;
   this.lcAnagram = anagram.toLowerCase();
@@ -23,7 +13,7 @@ Subject.prototype.matches = function (candidates) {
     candidatesAsArray = candidates;
 
   if (typeof candidates === 'string') {
-    candidatesAsArray = buildCandidatesAsArray(arguments);
+    candidatesAsArray = Array.prototype.slice.call(arguments);
   }
 
   return candidatesAsArray.filter(this.isAnagram, this);
