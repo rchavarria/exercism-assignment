0ee34f7e2fa56d4751d3274b7b8c65ea7eeb3bb6
Rubén Chavarría <txingo@gmail.com>
2015-07-30 08:46:31 +0200

    Refactor: remove duplication in the last lines of the verse

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -5,6 +5,21 @@
     return 'I know an old lady who swallowed a ' + animals[verseIndex - 1] + '.';
   }
 
+  function lastLines(verseIndex) {
+    var lines = [];
+
+    switch(verseIndex) {
+      case 3:
+        lines.push('She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.');
+      case 2:
+        lines.push('She swallowed the spider to catch the fly.');
+      case 1:
+        lines.push('I don\'t know why she swallowed the fly. Perhaps she\'ll die.');
+    }
+
+    return lines;
+  }
+
   function Song() { }
 
   Song.prototype.verse = function (verseIndex) {
@@ -12,23 +27,22 @@
 
     if (verseIndex === 1) {
       verses.push(firstLine(verseIndex));
-      verses.push('I don\'t know why she swallowed the fly. Perhaps she\'ll die.');
+      verses = verses.concat(lastLines(verseIndex));
     }
 
     if (verseIndex === 2) {
       verses.push(firstLine(verseIndex));
       verses.push('It wriggled and jiggled and tickled inside her.');
-      verses.push('She swallowed the spider to catch the fly.');
-      verses.push('I don\'t know why she swallowed the fly. Perhaps she\'ll die.');
+      verses = verses.concat(lastLines(verseIndex));
     }
 
     if (verseIndex === 3) {
       verses.push(firstLine(verseIndex));
       verses.push('How absurd to swallow a bird!');
-      verses.push('She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.');
-      verses.push('She swallowed the spider to catch the fly.');
-      verses.push('I don\'t know why she swallowed the fly. Perhaps she\'ll die.');
+      verses = verses.concat(lastLines(verseIndex));
     }
+
+    console.log(verses);
 
     verses.push('');
     return verses.join('\n');
