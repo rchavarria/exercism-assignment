270f26ffefd302cd38815ba94ef835cc13c981da
Rubén Chavarría <txingo@gmail.com>
2015-08-08 20:10:06 +0200

    Make *prints an arbitrary verse* test to pass

---
+++ javascript/beer-song/beer-song.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  verse: function (index) {
+    return "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n";
+  }
+};
