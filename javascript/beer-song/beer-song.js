027c7cc3249d83f556f6a6e34c82547a330441e3
Rubén Chavarría <txingo@gmail.com>
2015-08-09 18:56:42 +0200

    Refactor: use some tricks seen on exercism.io
    1. `splice(1)` instead of `substring(1, len)`
    2. use `replace` instead of concatenating strings

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,5 +1,15 @@
 'use strict';
 
+function format(message, params) {
+  return message
+    .replace(/\{0\}/g, params[0])
+    .replace(/\{1\}/g, params[1]);
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 function pluralize(n) {
   var suffix = (n === 1) ? '' : 's';
   return 'bottle' + suffix;
@@ -13,25 +23,27 @@ function bottles(n) {
   }
 }
 
+function bottlesOfBeer(n) {
+  return bottles(n) + ' ' + pluralize(n);
+}
+
 function start(n) {
-  var howMany = bottles(n) + ' ' + pluralize(n);
-  var result = howMany + ' of beer on the wall, ' + howMany + ' of beer.';
-  return result.charAt(0).toUpperCase() + result.substring(1, result.length);
+  var params = [ bottlesOfBeer(n) ];
+  var result = format('{0} of beer on the wall, {0} of beer.', params);
+  return capitalize(result);
 }
 
 function takeOneDown(n) {
   switch(n) {
-    case 1: return 'Take it down and pass it around, ';
-    case 0: return 'Go to the store and buy some more, ';
-    default: return 'Take one down and pass it around, ';
+    case 1: return 'Take it down and pass it around';
+    case 0: return 'Go to the store and buy some more';
+    default: return 'Take one down and pass it around';
   }
 }
 
 function end(n) {
-  var takeDown = takeOneDown(n);
-  var howMany = bottles(n - 1) + ' ' + pluralize(n - 1) + ' of beer on the wall.';
-
-  return takeDown + howMany;
+  var params = [ takeOneDown(n), bottlesOfBeer(n - 1) ];
+  return format('{0}, {1} of beer on the wall.', params);
 }
 
 module.exports = {
