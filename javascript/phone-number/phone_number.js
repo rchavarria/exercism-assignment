0cbea382b877ec4d96f3132174cf756d57144906
Rubén Chavarría <txingo@gmail.com>
2015-09-09 09:25:29 +0200

    Make *formats a number* test to pass

--- javascript/phone-number/phone_number.js
+++ javascript/phone-number/phone_number.js
@@ -19,6 +19,14 @@ function validate (number) {
   return validNumber.substring(1);
 }
 
+function middleCode(number) {
+  return number.substring(3, 6);
+}
+
+function endCode(number) {
+  return number.substring(6);
+}
+
 function PhoneNumber (number) {
   this.phoneNumber = validate(number);
 }
@@ -29,6 +37,18 @@ PhoneNumber.prototype.number = function () {
 
 PhoneNumber.prototype.areaCode = function () {
   return this.phoneNumber.substring(0, 3);
+}
+
+PhoneNumber.prototype.toString = function () {
+  return [
+      '(',
+      this.areaCode(),
+      ')',
+      ' ',
+      middleCode(this.phoneNumber),
+      '-',
+      endCode(this.phoneNumber)
+    ].join('');
 }
 
 module.exports = PhoneNumber;
