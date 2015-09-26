2f31545b86156b91a70a0af7498824a7e4260ecb
Rubén Chavarría <txingo@gmail.com>
2015-09-26 17:37:38 +0200

    Refactor: make code a bit cleaner

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,16 +1,16 @@
-var AVAILABLE_POINTS = [ '1', '2', '3', '4', '5', '8', '10' ];
+var AVAILABLE_POINTS = [ 1, 2, 3, 4, 5, 8, 10 ];
 
-function transform(old) {
+function transform(oldStructure) {
   var newStructure = {};
 
   AVAILABLE_POINTS.forEach(function (point) {
-    var characters = old[point];
+    var characters = oldStructure[point];
     if (!characters) {
       return;
     }
 
     characters.forEach(function (c) {
-      newStructure[c.toLowerCase()] = parseInt(point, 10);
+      newStructure[c.toLowerCase()] = point;
     });
   });
 
