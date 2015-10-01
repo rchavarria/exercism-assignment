9d88937060bba8c88d7860be1884ab2965241283
Rubén Chavarría <txingo@gmail.com>
2015-10-01 15:17:22 +0200

    Make the rest of the tests pass. Just add new methods and some data in the constant

--- javascript/space-age/space_age.js
+++ javascript/space-age/space_age.js
@@ -2,7 +2,12 @@ var SECONDS_IN_AN_EARTH_DAY = 24 * 60 * 60,
   EARTH_DAYS_IN_A_YEAR = {
     'Mercury': 87.97,
     'Earth': 365.25,
-    'Venus': 224.7
+    'Venus': 224.7,
+    'Mars': 687,
+    'Jupiter': 4331.86,
+    'Saturn': 10760.26,
+    'Uranus': 30706.57,
+    'Neptune': 60193.2
   };
 
 function ageOn(planet, seconds) {
@@ -24,6 +29,26 @@ SpaceAge.prototype.onMercury = function () {
 
 SpaceAge.prototype.onVenus = function () {
   return ageOn('Venus', this.seconds);
+}
+
+SpaceAge.prototype.onMars = function () {
+  return ageOn('Mars', this.seconds);
+}
+
+SpaceAge.prototype.onJupiter = function () {
+  return ageOn('Jupiter', this.seconds);
+}
+
+SpaceAge.prototype.onSaturn = function () {
+  return ageOn('Saturn', this.seconds);
+}
+
+SpaceAge.prototype.onUranus = function () {
+  return ageOn('Uranus', this.seconds);
+}
+
+SpaceAge.prototype.onNeptune = function () {
+  return ageOn('Neptune', this.seconds);
 }
 
 module.exports = SpaceAge;
