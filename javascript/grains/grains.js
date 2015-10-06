3e5c6addc9154e959281cab3824cca81ae5528c0
Rubén Chavarría <txingo@gmail.com>
2015-10-06 09:29:31 +0200

    Make *total* test pass

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -7,4 +7,18 @@ Grains.prototype.square = function (n) {
   return new BigInt(2).pow(n - 1).toString();
 }
 
+Grains.prototype.total = function () {
+  var i = 1,
+    square,
+    sum = new BigInt(0);
+
+  while (i <= 64) {
+    square = this.square(i);
+    sum = sum.add(square);
+    i++;
+  }
+
+  return sum.toString();
+}
+
 module.exports = Grains;
--- javascript/grains/grains_test.spec.js
+++ javascript/grains/grains_test.spec.js
@@ -61,7 +61,7 @@ describe('Grains', function () {
     expect(grains.square(64)).toBe('9223372036854775808');
   });
 
-  xit('total', function () {
+  it('total', function () {
     expect(grains.total()).toBe('18446744073709551615');
   });
 });
