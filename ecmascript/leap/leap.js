ad30ed5ce5dafcfc4a43f5026d7bacfe899e54d1
Rubén Chavarría <txingo@gmail.com>
2015-10-10 22:23:38 +0200

    Refactor: extract method to check if a number is divisible or not

--- ecmascript/leap/leap.js
+++ ecmascript/leap/leap.js
@@ -1,12 +1,16 @@
+function divisibleBy(year, n) {
+  return year % n === 0;
+}
+
 export default function isLeapYear(year) {
-  if (year % 400 === 0) {
+  if (divisibleBy(year, 400)) {
     return true;
   }
 
-  if (year % 100 === 0) {
+  if (divisibleBy(year, 100)) {
     return false;
   }
 
-  return year % 4 === 0;
+  return divisibleBy(year, 4);
 }
 
