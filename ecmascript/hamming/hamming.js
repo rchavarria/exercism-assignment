37d46167d855a6e876fe4e945abc53bf57f6f46d
Rubén Chavarría <txingo@gmail.com>
2015-10-12 20:33:30 +0200

    Make *throws error when strands are not equal length* test pass

--- ecmascript/hamming/hamming.js
+++ ecmascript/hamming/hamming.js
@@ -1,7 +1,10 @@
 export default class Hamming {
   compute(strand1, strand2) {
-    let difference = 0;
+    if (strand1.length !== strand2.length) {
+      throw new Error('DNA strands must be of equal length.');
+    }
 
+    let difference = 0;
     for (let i = 0; i < strand1.length; i++) {
       if (strand1.charAt(i) === strand2.charAt(i)) {
         continue;
