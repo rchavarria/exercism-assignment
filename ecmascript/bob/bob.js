72baa584e8a8672f9cfe3e9acc6d1fd036dbaa6b
Rubén Chavarría <txingo@gmail.com>
2015-10-17 09:10:46 +0200

    Make *stating something* test pass

--- ecmascript/bob/bob.js
+++ ecmascript/bob/bob.js
@@ -1,13 +1,6 @@
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 class Bob {
   hey(message) {
-    //
-    // YOUR CODE GOES HERE
-    //
+    return 'Whatever.';
   }
 }
 
