66f8db0fd9c8e287a5670b8c308089b49c315b82
Rubén Chavarría <txingo@gmail.com>
2015-10-19 23:18:58 +0200

    Refactor: remove the need of ifs statements. Use destructuring to get the first element of an array and an attribute of an object

--- ecmascript/bob/bob.js
+++ ecmascript/bob/bob.js
@@ -1,22 +1,20 @@
 let isSilent = (m) => m.trim().length === 0;
-let isShouting = (m) => m.toUpperCase() === m && m.match(/[A-Z]/);
+let isShouting = (m) => m.toUpperCase() === m && m.match(/[A-Z\xc0-\xdf]/);
 let isAsking = (m) => m.endsWith('?');
+let anything = () => true;
+
+const CONDITIONS = [
+  { question: isSilent, response: 'Fine. Be that way!' },
+  { question: isShouting, response: 'Whoa, chill out!' },
+  { question: isAsking, response: 'Sure.' },
+  { question: anything, response: 'Whatever.' }
+];
 
 class Bob {
   hey(message) {
-    if (isSilent(message)) {
-      return 'Fine. Be that way!';
-    }
-
-    if (isShouting(message)) {
-      return 'Whoa, chill out!';
-    }
-
-    if (isAsking(message)) {
-      return 'Sure.';
-    }
-
-    return 'Whatever.';
+    message = message.trim();
+    let [ {response} ] = CONDITIONS.filter((c) => c.question(message));
+    return response;
   }
 }
 
