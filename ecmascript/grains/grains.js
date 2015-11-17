6307f9bbe289237605cf21929488ffed50dfff63
Rubén Chavarría <txingo@gmail.com>
2015-11-17 23:28:43 +0100

    Refactor Grains to a *functional* style

--- ecmascript/grains/grains.js
+++ ecmascript/grains/grains.js
@@ -1,13 +1,10 @@
 import BigInt from './big-integer';
 
-class Grains {
-  square(n) {
-    return BigInt(2).pow(n - 1).toString();
-  }
+function Grains() {
+  const square = n => BigInt(2).pow(n - 1).toString();
+  const total = () => BigInt(2).pow(64).minus(1).toString();
 
-  total() {
-    return BigInt(2).pow(64).minus(1).toString();
-  }
+  return Object.freeze({ square, total });
 }
 
 export default Grains;
