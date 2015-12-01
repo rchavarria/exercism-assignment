1c23cd6b956d0b42222469e68bcc37ade481521e
Rubén Chavarría <txingo@gmail.com>
2015-12-01 22:31:31 +0100

    Refactor Grains, remove duplicated code

--- ecmascript/grains/grains.js
+++ ecmascript/grains/grains.js
@@ -1,10 +1,13 @@
 import BigInt from './big-integer';
 
+const powerOf2 = (exp) => BigInt(2).pow(exp);
+
 function Grains() {
-  const square = n => BigInt(2).pow(n - 1).toString();
-  const total = () => BigInt(2).pow(64).minus(1).toString();
+  const square = n => powerOf2(n - 1).toString();
+  const total = () => powerOf2(64).minus(1).toString();
 
   return Object.freeze({ square, total });
 }
 
 export default Grains;
+
