3bcbab1acb25810f5634a5ac77b041553346dca8
Rubén Chavarría <txingo@gmail.com>
2015-12-29 20:21:24 +0100

    Scrabble score: a second try

--- ecmascript/scrabble-score/scrabble-score.js
+++ ecmascript/scrabble-score/scrabble-score.js
@@ -1,28 +1,34 @@
-const POINTS = {
-  1: [ 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' ],
-  2: [ 'D', 'G' ],
-  3: [ 'B', 'C', 'M', 'P' ],
-  4: [ 'F', 'H', 'V', 'W', 'Y' ],
-  5: [ 'K' ],
-  8: [ 'J', 'X' ],
-  10: [ 'Q', 'Z' ]
-};
+const POINTS_BY_CHAR = {
+  'A': 1,
+  'B': 3,
+  'C': 3,
+  'D': 2,
+  'E': 1,
+  'F': 4,
+  'G': 2,
+  'H': 4,
+  'I': 1,
+  'J': 8,
+  'K': 5,
+  'L': 1,
+  'M': 3,
+  'N': 1,
+  'O': 1,
+  'P': 3,
+  'Q': 10,
+  'R': 1,
+  'S': 1,
+  'T': 1,
+  'U': 1,
+  'V': 4,
+  'W': 4,
+  'X': 8,
+  'Y': 4,
+  'Z': 10
+}
 
 export default (word) => {
-  if (!word) {
-    return 0;
-  }
-
-  let total = 0;
-  [...word.toUpperCase()].forEach(i => {
-    Object.keys(POINTS).forEach(p => {
-      const chars = POINTS[p];
-      if (chars.some(c => c === i)) {
-        total += parseInt(p, 10);
-      }
-    });
-  });
-
-  return total;
+  return [...(word || '').toUpperCase()]
+    .reduce((total, i) => total + POINTS_BY_CHAR[i], 0);
 }
 
