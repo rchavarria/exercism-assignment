2fe2e82d4a072c2c31387a613ad59e934de9ab38
Rubén Chavarría <txingo@gmail.com>
2015-12-29 19:47:21 +0100

    Clock: implement it in a classy style

--- ecmascript/clock/clock.js
+++ ecmascript/clock/clock.js
@@ -4,17 +4,30 @@ const MINUTES_IN_A_DAY = HOURS_IN_A_DAY * MINUTES_IN_AN_HOUR;
 
 const pad = (i) => `00${i}`.slice(-2);
 
-function at(hours, minutes = 0) {
-  const totalMinutes = (hours * 60 + minutes + MINUTES_IN_A_DAY) % MINUTES_IN_A_DAY;
-  const h = parseInt(totalMinutes / 60) % 24;
-  const m = totalMinutes % 60;
+class Clock {
+  constructor(hours, minutes) {
+    this.totalMinutes = (hours * MINUTES_IN_AN_HOUR + minutes + MINUTES_IN_A_DAY) % MINUTES_IN_A_DAY;
+  }
 
-  const toString = () => `${pad(h)}:${pad(m)}`;
-  const plus = (minutesToAdd) => at(hours, minutes + minutesToAdd);
-  const minus = (minutesToSubstract) => plus(-minutesToSubstract);
-  const equals = (other) => toString() === other.toString();
-  return { toString, plus, minus, equals };
+  toString() {
+    const h = parseInt(this.totalMinutes / MINUTES_IN_AN_HOUR) % HOURS_IN_A_DAY;
+    const m = this.totalMinutes % MINUTES_IN_AN_HOUR;
+
+    return `${pad(h)}:${pad(m)}`;
+  }
+
+  plus(minutesToAdd) {
+    return new Clock(0, this.totalMinutes + minutesToAdd);
+  }
+
+  minus(minutesToSubstract) {
+    return this.plus(-minutesToSubstract);
+  }
+
+  equals(otherClock) {
+    return this.toString() === otherClock.toString();
+  }
 }
 
-export default at;
+export default (hours, minutes = 0) => new Clock(hours, minutes);
 
