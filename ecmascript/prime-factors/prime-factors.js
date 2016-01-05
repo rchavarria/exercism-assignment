0247ebe989f5b02ec0db60246e23ceb10b9a6399
Rubén Chavarría <txingo@gmail.com>
2016-01-05 10:21:47 +0100

    Solved exercise: Prime factors

---
+++ ecmascript/prime-factors/prime-factors.js
@@ -0,0 +1,21 @@
+export default class PrimeFactors {
+  for(n) {
+    let factors = [];
+    let candidate = 2;
+
+    while (candidate < n) {
+      if (n % candidate === 0) {
+        factors.push(candidate);
+        n /= candidate;
+      } else {
+        candidate++;
+      }
+    }
+
+    if (n > 1) {
+      factors.push(n);
+    }
+
+    return factors;
+  }
+}
