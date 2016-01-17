bf6c854563d867c1c8c1a2800ecd216632159088
Rubén Chavarría <txingo@gmail.com>
2016-01-17 18:28:34 +0100

    Allergies: solved

---
+++ ecmascript/allergies/allergies.js
@@ -0,0 +1,33 @@
+const POSSIBLE_ALLERGIES = [
+  'eggs',
+  'peanuts',
+  'shellfish',
+  'strawberries',
+  'tomatoes',
+  'chocolate',
+  'pollen',
+  'cats'
+];
+
+function listOfAllergies(allergies) {
+  return POSSIBLE_ALLERGIES
+    .filter((_, idx) => allergies & Math.pow(2, idx))
+    .map(candidate => candidate);
+}
+
+class Allergies {
+  constructor(allergies) {
+    this.listOfAllergies = listOfAllergies(allergies);
+  }
+
+  list() {
+    return this.listOfAllergies;
+  }
+  
+  allergicTo(allergy) {
+    return this.listOfAllergies.includes(allergy);
+  }
+}
+
+export default Allergies;
+
