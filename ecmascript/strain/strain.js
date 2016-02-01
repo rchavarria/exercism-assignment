a1b7fc8e0fc0104abc2fe16fe4311559235ae075
Rubén Chavarría <txingo@gmail.com>
2016-02-01 23:15:40 +0100

    Strain: implementing my own filter function

--- ecmascript/strain/strain.js
+++ ecmascript/strain/strain.js
@@ -1,4 +1,19 @@
-const keep = (l, f) => l.filter(f);
-const discard = (l, f) => l.filter(e => !f(e));
+const filter = (list, predicate) => {
+  let result = [];
+
+  const len = list.length;
+  for (let i = 0; i < len; i++) {
+    let element = list[i];
+    if (predicate(element)) {
+      result.push(element);
+    }
+  }
+
+  return result;
+}
+
+const keep = (list, predicate) => filter(list, predicate);
+const discard = (list, predicate) => filter(list, e => !predicate(e));
+
 export default Object.freeze({ keep, discard });
 
