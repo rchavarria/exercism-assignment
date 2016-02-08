c696e54a4ee93a4f0486541f6162d973c645ea51
Rubén Chavarría <txingo@gmail.com>
2016-02-08 21:24:20 +0100

    Strain: use a function that builds the filter function

--- ecmascript/strain/strain.js
+++ ecmascript/strain/strain.js
@@ -1,19 +1,21 @@
-const filter = (list, predicate) => {
-  let result = [];
+function filterBuilder(keep) {
+  return (list, predicate) => {
+    let result = [];
 
-  const len = list.length;
-  for (let i = 0; i < len; i++) {
-    let element = list[i];
-    if (predicate(element)) {
-      result.push(element);
+    const len = list.length;
+    for (let i = 0; i < len; i++) {
+      let element = list[i];
+      if (predicate(element) === keep) {
+        result.push(element);
+      }
     }
-  }
 
-  return result;
+    return result;
+  }
 }
 
-const keep = (list, predicate) => filter(list, predicate);
-const discard = (list, predicate) => filter(list, e => !predicate(e));
-
-export default Object.freeze({ keep, discard });
+export default Object.freeze({
+  keep: filterBuilder(true),
+  discard: filterBuilder(false)
+});
 
