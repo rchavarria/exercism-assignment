c37d182f1bcb05d1001cddf3f1bbc8e4893c2ad5
Rubén Chavarría <txingo@gmail.com>
2016-02-21 20:07:02 +0100

    Atbash cipher: improve how to split a string into 5-char-long chunks

--- ecmascript/atbash-cipher/atbash-cipher.js
+++ ecmascript/atbash-cipher/atbash-cipher.js
@@ -2,18 +2,16 @@ const ALPHABET = 'abcdefghijklmnopqrstuvwxyz1234567890';
 const ATBASH_ALPHABET = 'zyxwvutsrqponmlkjihgfedcba1234567890';
 const ATBASH_GROUP_SIZE = 5;
 const CLEAR_PHRASE_REGEXP = /[\s\,\.]/g;
+const SPLIT_IN_CHUNKS_REGEXP = /.{1,5}/g;
 
 function encode(phrase) {
   const clearPhrase = phrase.replace(CLEAR_PHRASE_REGEXP, '').toLowerCase();
   let encoded = [...clearPhrase]
     .map(chr => ALPHABET.indexOf(chr))
-    .map(i => ATBASH_ALPHABET[i]);
+    .map(i => ATBASH_ALPHABET[i])
+    .join('');
 
-  for(let i = ATBASH_GROUP_SIZE; i < encoded.length; i += ATBASH_GROUP_SIZE + 1) {
-    encoded.splice(i, 0, ' ');
-  }
-
-  return encoded.join('');
+  return encoded.match(SPLIT_IN_CHUNKS_REGEXP).join(' ');
 }
 
 export default Object.freeze({ encode });
