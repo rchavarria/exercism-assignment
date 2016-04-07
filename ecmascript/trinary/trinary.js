f9e7564cf8297838dc34e5b186eb8453025caa01
Rubén Chavarría <txingo@gmail.com>
2016-04-07 22:40:49 +0200

    Trinary: use a different method to compute the value
    @matthewmorgan (and others) compute the decimal number
    in a different way, that doesn't need to reverse the digits, so I just
    wanted to give it a try.
    
    It does not simplify the solution as much as I expected, but it's fun
    playing with other approaches.

--- ecmascript/trinary/trinary.js
+++ ecmascript/trinary/trinary.js
@@ -1,16 +1,13 @@
+const NOT_ALLOWED_CHARS = /[^0-9]/g;
+
 function Trinary(trinary) {
-  const NOT_ALLOWED_CHARS = /[^0-9]/g;
   
   function toDecimal() {
     if (NOT_ALLOWED_CHARS.test(trinary)) {
       return 0;
     }
 
-    return [...trinary]
-      .reverse()
-      .map(d => parseInt(d, 10))
-      .map((d, i) => d * (3 ** i))
-      .reduce((sum, d) => sum + d, 0);
+    return [...trinary].reduce((total, i) => +i + total * 3, 0);
   }
 
   return Object.freeze({ toDecimal });
