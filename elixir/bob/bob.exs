364a89c4126151a8a28a73b5b459d5a01f83fa14
Rubén Chavarría <txingo@gmail.com>
2016-06-01 18:29:38 +0200

    Bob on Elixir: solve some more test cases

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,8 +1,9 @@
 defmodule Bob do
   def hey(input) do
     cond do
+        String.ends_with?(input, "?") -> "Sure."
+        String.upcase(input) == input -> "Whoa, chill out!"
         true -> "Whatever."
-
     end
   end
 end
