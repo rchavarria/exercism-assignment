6d712551cdce679da065dc3a579955edd325b31d
Rubén Chavarría <txingo@gmail.com>
2016-06-28 01:37:10 +0200

    Bob on Elixir: refactor the solution after stealing a few ideas

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,10 +1,16 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      String.strip(input) == "" -> "Fine. Be that way!"
-      String.upcase(input) == input and Regex.match?(~r/[A-Z\xc0-\xdf]/, input) -> "Whoa, chill out!"
-      String.ends_with?(input, "?") -> "Sure."
-      true -> "Whatever."
+      silence?(input) -> "Fine. Be that way!"
+      shouting?(input) -> "Whoa, chill out!"
+      asking?(input) -> "Sure."
+      talking?(input) -> "Whatever."
     end
   end
+
+  defp silence?(input), do: String.strip(input) == ""
+  defp shouting?(input), do: String.upcase(input) == input and String.downcase(input) != input
+  defp asking?(input), do: String.ends_with?(input, "?")
+  defp talking?(input), do: true
+
 end
