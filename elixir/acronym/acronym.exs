a3b4fe7c249a4f23dfd7c2c31c1987c00e787b80
Rubén Chavarría <txingo@gmail.com>
2016-07-01 12:30:10 +0200

    Acronym on Elixir: solve the exercise

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -5,6 +5,12 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
-    
+    string
+    |> String.replace(~r{([A-Z])}, " \\1")
+    |> String.split(~r{[[:blank:]-]})
+    |> Enum.filter(fn w -> String.length(w) > 0 end)
+    |> Enum.map(&String.first/1)
+    |> Enum.map(&String.upcase/1)
+    |> Enum.join
   end
 end
--- elixir/acronym/acronym_test.exs
+++ elixir/acronym/acronym_test.exs
@@ -12,22 +12,18 @@ defmodule AcronymTest do
     assert Acronym.abbreviate("Portable Networks Graphic") === "PNG"
   end
 
-  @tag :pending
   test "it produces acronyms from lower case" do
     assert Acronym.abbreviate("Ruby on Rails") === "ROR"
   end
 
-  @tag :pending
   test "it produces acronyms from inconsistent case" do
     assert Acronym.abbreviate("HyperText Markup Language") === "HTML"
   end
 
-  @tag :pending
   test "it ignores punctuation" do
     assert Acronym.abbreviate("First in, First out") === "FIFO"
   end
 
-  @tag :pending
   test "produces acronyms ignoring punctuation and casing" do
     assert Acronym.abbreviate("Complementary Metal-Oxide semiconductor") === "CMOS"
   end
