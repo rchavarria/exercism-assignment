74a4eb32debfc441e258c99dff0147b50945b20f
Rubén Chavarría <txingo@gmail.com>
2016-07-01 12:41:16 +0200

    Acronym on Elixir: extract small methods to make it clearer

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -6,11 +6,29 @@ defmodule Acronym do
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
     string
-    |> String.replace(~r{([A-Z])}, " \\1")
-    |> String.split(~r{[[:blank:]-]})
-    |> Enum.filter(fn w -> String.length(w) > 0 end)
-    |> Enum.map(&String.first/1)
-    |> Enum.map(&String.upcase/1)
+    |> highlight_mixed_capital_letters
+    |> split_into_non_empty_words
+    |> extract_firts_capitalized_letter
     |> Enum.join
   end
+
+  defp highlight_mixed_capital_letters(string) do
+    capture_capital_letter = ~r{([A-Z])}
+    String.replace(string, capture_capital_letter, " \\1")
+  end
+
+  defp split_into_non_empty_words(string) do
+    word_separator = ~r{[[:blank:]-]}
+
+    string
+    |> String.split(word_separator)
+    |> Enum.filter(&(String.length(&1) > 0))
+  end
+
+  defp extract_firts_capitalized_letter(non_empty_words) do
+    non_empty_words
+    |> Enum.map(&String.first/1)
+    |> Enum.map(&String.upcase/1)
+  end
+
 end
