0c6781b3986380ccf3bb3b8d488d2179ec0e8f86
Rubén Chavarría <txingo@gmail.com>
2016-07-08 09:26:23 +0200

    Learn about `Enum.map_join`

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -5,30 +5,15 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
-    string
-    |> highlight_mixed_capital_letters
-    |> split_into_non_empty_words
-    |> extract_firts_capitalized_letter
-    |> Enum.join
-  end
-
-  defp highlight_mixed_capital_letters(string) do
     capture_capital_letter = ~r{([A-Z])}
-    String.replace(string, capture_capital_letter, " \\1")
-  end
-
-  defp split_into_non_empty_words(string) do
     word_separator = ~r{[[:blank:]-]}
 
     string
+    |> String.replace(capture_capital_letter, " \\1")
     |> String.split(word_separator)
     |> Enum.filter(&(String.length(&1) > 0))
-  end
-
-  defp extract_firts_capitalized_letter(non_empty_words) do
-    non_empty_words
-    |> Enum.map(&String.first/1)
-    |> Enum.map(&String.upcase/1)
+    |> Enum.map_join("", &String.first/1)
+    |> String.upcase
   end
 
 end
