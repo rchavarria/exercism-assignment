5c2c4e16bf8b4b3ddf789a8f5654a95438ee8217
Rubén Chavarría <txingo@gmail.com>
2016-07-13 08:52:01 +0200

    Run length encoding on Elixir: use `Enum.chunk_by/2`
    It definitely makes the code cleaner, it does the right
    thing for this exercise

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -8,30 +8,11 @@ defmodule RunLengthEncoder do
   "1H1O1R1S1E" => "HORSE"
   """
   @spec encode(String.t) :: String.t
-  def encode(""), do: ""
   def encode(string) do
-    { count_list, _ } = string
-      |> String.graphemes
-      |> Enum.reduce({ [], "" }, &count_similar_chars/2)
-
-    { result_string, _ } = count_list
-      |> Enum.reverse
-      |> Enum.reduce({ "", 0 }, fn (count, { result_string, index }) ->
-        result_string = result_string <> Integer.to_string(count) <> String.at(string, index)
-        { result_string, index + count }
-      end)
-    result_string
-  end
-
-  defp count_similar_chars(current_char, { [], "" }) do
-    { [1], current_char }
-  end
-  defp count_similar_chars(current_char, { count_list, current_char }) do
-    [ current_count | tail ] = count_list
-    { [ current_count + 1 | tail ], current_char }
-  end
-  defp count_similar_chars(current_char, { count_list, _ }) do
-    { [ 1 | count_list ], current_char }
+    string
+    |> String.graphemes
+    |> Enum.chunk_by(&(&1))
+    |> Enum.map_join(fn(x) -> "#{length(x)}#{List.first(x)}" end)
   end
 
   @spec decode(String.t) :: String.t
