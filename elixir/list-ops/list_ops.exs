a2482030b5bfce9685462e0ff0e956ad498560c3
Rubén Chavarría <txingo@gmail.com>
2016-07-15 08:55:54 +0200

    List ops on Elixir: solved exercise

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -22,27 +22,57 @@ defmodule ListOps do
 
   @spec map(list, (any -> any)) :: list
   def map(l, f) do
-
+    _map(l, f)
+  end
+  defp _map([], f), do: []
+  defp _map([ head | tail ], f) do
+    mapped_head = f.(head)
+    [ mapped_head | _map(tail, f) ]
   end
 
   @spec filter(list, (any -> as_boolean(term))) :: list
   def filter(l, f) do
-
+    _filter(l, f)
+  end
+  defp _filter([], f), do: []
+  defp _filter([ head | tail ], f) do
+    if f.(head) do
+      [ head | _filter(tail, f) ]
+    else
+      _filter(tail, f)
+    end
   end
 
   @type acc :: any
   @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
   def reduce(l, acc, f) do
-
+    _reduce(l, acc, f)
+  end
+  defp _reduce([], acc, _), do: acc
+  defp _reduce([ head | tail ], acc, f) do
+    next_acc = f.(head, acc)
+    _reduce(tail, next_acc, f)
   end
 
   @spec append(list, list) :: list
   def append(a, b) do
-
+    _append(a, b)
+  end
+  defp _append([], []), do: []
+  defp _append(a, []), do: a
+  defp _append([], b), do: b
+  defp _append([ head | tail ], b) do
+    [ head | _append(tail, b) ]
   end
 
   @spec concat([[any]]) :: [any]
   def concat(ll) do
-
+    _concat(ll)
   end
+  defp _concat([]), do: []
+  defp _concat([ [] | rest ]), do: _concat(rest)
+  defp _concat([ [ head | tail ] | rest ]) do
+    [ head | _concat([ tail | rest ]) ]
+  end
+
 end
