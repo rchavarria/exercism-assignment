c1f512c131c2f9d7e031ea5586d63631007062c4
Rubén Chavarría <txingo@gmail.com>
2016-08-10 08:23:02 +0200

    Sublist on Elixir: refactor with some ideas from other users
    Looking at other users' solutions, I realize I don't have to *loop* over all
    elements in the lists to know if they are equal, it's enough to use
    
        a === b
    
    or even pattern matching
    
        def compare(a, a), do: :equal
    
    Other users use `Stream.chunk` to do what I did with several methods and
    recursion. It's much easier with `chunk`, it gives you a list of lists, each
    one with the length you specified. Then, one just has to compare with `===` to
    know if a sublist is equal to a list. Impresive!

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -3,37 +3,24 @@ defmodule Sublist do
   def compare([], []), do: :equal
   def compare([], _), do: :sublist
   def compare(_, []), do: :superlist
-  def compare(a, b), do: compare_lists(a, b, :equal)
-
-  defp compare_lists(a, b, comparison_result) when length(a) == length(b) do
-    compare_same_length_lists(a, b, comparison_result)
+  def compare(a, a), do: :equal
+  def compare(a, b) when length(a) == length(b), do: :unequal
+  def compare(a, b) when length(a) < length(b) do
+    cond do
+      is_sublist?(a, b) -> :sublist
+      true -> :unequal
+    end
   end
-  defp compare_lists(a, b, :equal) when length(a) < length(b) do
-    compare_different_length_lists(a, b, :sublist)
-  end
-  defp compare_lists(a, b, comparison_result) when length(a) < length(b) do
-    compare_different_length_lists(a, b, comparison_result)
-  end
-  defp compare_lists(a, b, :equal) when length(a) > length(b) do
-    compare_different_length_lists(b, a, :superlist)
-  end
-  defp compare_lists(a, b, comparison_result) when length(a) > length(b) do
-    compare_different_length_lists(b, a, comparison_result)
-  end
-
-  defp compare_different_length_lists(a, b, comparison_result) do
-    [ _ | tail ] = b
-    smaller_list = Enum.take(b, length(a))
-    case compare_same_length_lists(a, smaller_list, comparison_result) do
-      :unequal -> compare_lists(a, tail, comparison_result)
-      _ -> comparison_result
+  def compare(a, b) when length(a) > length(b) do
+    cond do
+      is_sublist?(b, a) -> :superlist
+      true -> :unequal
     end
   end
 
-  defp compare_same_length_lists([], [], comparison_result), do: comparison_result
-  defp compare_same_length_lists([head | a], [head | b], comparison_result) do
-    compare_same_length_lists(a, b, comparison_result)
+  defp is_sublist?(a, b) do
+    Stream.chunk(b, length(a), 1)
+    |> Enum.any?(&(&1 === a))
   end
-  defp compare_same_length_lists(_, _, _), do: :unequal
 
 end
