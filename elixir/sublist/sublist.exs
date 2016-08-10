972526e7bae767a3dfdc104ee8ce3c58c4178586
Rubén Chavarría <txingo@gmail.com>
2016-08-10 08:46:36 +0200

    Sublist on Elixir: remove all `cond` statements
    Make use of pattern matching to remove all `cond` statements.
    
    Method names are not the best ones, but I hope they explain what the
    code does. `is_first_list_longer` helps to decide which list
    could be contained in which list. Then, `which_list_is_contained`
    returns a result, depending on which list was longer. Finally,
    `extract_result` returns the right value, extracting it with
    pattern matching.

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -5,22 +5,27 @@ defmodule Sublist do
   def compare(_, []), do: :superlist
   def compare(a, a), do: :equal
   def compare(a, b) when length(a) == length(b), do: :unequal
-  def compare(a, b) when length(a) < length(b) do
-    cond do
-      is_sublist?(a, b) -> :sublist
-      true -> :unequal
-    end
-  end
-  def compare(a, b) when length(a) > length(b) do
-    cond do
-      is_sublist?(b, a) -> :superlist
-      true -> :unequal
-    end
+  def compare(a, b) do
+    is_first_list_longer?(a, b)
+    |> which_list_is_contained(a, b)
+    |> extract_result
   end
 
+  defp is_first_list_longer?(a, b), do: length(a) < length(b)
+
+  defp which_list_is_contained(true, a, b) do
+    { is_sublist?(a, b), :sublist } 
+  end
+  defp which_list_is_contained(false, a, b) do
+    { is_sublist?(b, a), :superlist} 
+  end
+  
   defp is_sublist?(a, b) do
     Stream.chunk(b, length(a), 1)
     |> Enum.any?(&(&1 === a))
   end
+
+  defp extract_result({ true, comparison_result }), do: comparison_result
+  defp extract_result(_), do: :unequal
 
 end
