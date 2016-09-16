1d4a322ed4dbe37f067c5aaea91d34e5bc3cf45c
Rubén Chavarría <txingo@gmail.com>
2016-09-16 09:12:48 +0200

    Scrabble score on Elixir: solved exercise
    It's just a matter of refactoring to get it right.
    
    Probably, scores could be a more complex data structure,
    to make it more concise. But for now, it's very simple,
    it's easy to add new characters or modify the score of
    one of them.

--- elixir/scrabble-score/scrabble_score_test.exs
+++ elixir/scrabble-score/scrabble_score_test.exs
@@ -13,37 +13,37 @@ defmodule ScrabbleScoreTest do
     assert Scrabble.score("") == 0
   end
 
-  @tag :pending
+  # @tag :pending
   test "whitespace scores zero" do
     assert Scrabble.score(" \t\n") == 0
   end
 
-  @tag :pending
+  # @tag :pending
   test "scores very short word" do
     assert Scrabble.score("a") == 1
   end
 
-  @tag :pending
+  # @tag :pending
   test "scores other very short word" do
     assert Scrabble.score("f") == 4
   end
 
-  @tag :pending
+  # @tag :pending
   test "simple word scores the number of letters" do
     assert Scrabble.score("street") == 6
   end
 
-  @tag :pending
+  # @tag :pending
   test "complicated word scores more" do
     assert Scrabble.score("quirky") == 22
   end
 
-  @tag :pending
+  # @tag :pending
   test "scores are case insensitive" do
     assert Scrabble.score("OXYPHENBUTAZONE") == 41
   end
 
-  @tag :pending
+  # @tag :pending
   test "convenient scoring" do
     assert Scrabble.score("alacrity") == 13
   end
