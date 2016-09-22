daa3802a11b0ce8f9da68c7bb7f43d2547be4e31
Rubén Chavarría <txingo@gmail.com>
2016-09-22 08:59:47 +0200

    Anagram on Elixir: extract method `not_source_word`

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -6,8 +6,12 @@ defmodule Anagram do
   def match(base, candidates) do
     anagram_to_search = hash(base)
     candidates
-    |> Enum.filter(&(String.downcase(&1) != base))
+    |> Enum.filter(&(not_source_word(&1, base)))
     |> Enum.filter(&(hash(&1) == anagram_to_search))
+  end
+
+  defp not_source_word(candidate, source) do
+    String.downcase(candidate) != source
   end
 
   defp hash(word), do: word |> String.downcase |> String.graphemes |> Enum.sort |> Enum.join
