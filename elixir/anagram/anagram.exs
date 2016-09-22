90404ca0fce4b5fbdc30792d110727acde40c75b
Rubén Chavarría <txingo@gmail.com>
2016-09-22 09:01:53 +0200

    Anagram on Elixir: extract method that checks if a candidate matches the source word

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,14 +4,17 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-    anagram_to_search = hash(base)
     candidates
     |> Enum.filter(&(not_source_word(&1, base)))
-    |> Enum.filter(&(hash(&1) == anagram_to_search))
+    |> Enum.filter(&(candidate_matches_word(&1, base)))
   end
 
   defp not_source_word(candidate, source) do
     String.downcase(candidate) != source
+  end
+
+  defp candidate_matches_word(candidate, source) do
+    hash(candidate) == hash(source)
   end
 
   defp hash(word), do: word |> String.downcase |> String.graphemes |> Enum.sort |> Enum.join
