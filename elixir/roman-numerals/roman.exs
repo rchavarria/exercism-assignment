ab50ce195119c758e74b4b5a32f6c4e38b36f460
Rubén Chavarría <txingo@gmail.com>
2016-10-07 09:26:25 +0200

    Roman numerals on Elixir: solved exercise
    I don't like very much the solution. It works. Ok. But I don't
    think it's efficient, nor *elegant*. It just generates a bunch
    of `I`s and then it goes replacing *symbols*.
    
    Anyway, it's a very fun solution, but I'll try another approach,
    more algorithmic-ish one.

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -1,8 +1,21 @@
 defmodule Roman do
-  @doc """
-  Convert the number to a roman number.
-  """
   @spec numerals(pos_integer) :: String.t
   def numerals(number) do
+    1..number
+    |> Enum.map_join(fn _ -> "I" end)
+    |> String.replace("IIIII", "V")
+    |> String.replace("VV", "X")
+    |> String.replace("VIIII", "IX")
+    |> String.replace("IIII", "IV")
+
+    |> String.replace("XXXXX", "L")
+    |> String.replace("LL", "C")
+    |> String.replace("LXXXX", "XC")
+    |> String.replace("XXXX", "XL")
+
+    |> String.replace("CCCCC", "D")
+    |> String.replace("DD", "M")
+    |> String.replace("DCCCC", "CM")
+    |> String.replace("CCCC", "CD")
   end
 end
--- elixir/roman-numerals/roman_numerals_test.exs
+++ elixir/roman-numerals/roman_numerals_test.exs
@@ -13,87 +13,87 @@ defmodule RomanTest do
     assert Roman.numerals(1) == "I"
   end
 
-  @tag :skip
+  # @tag :skip
   test "2" do
     assert Roman.numerals(2) == "II"
   end
 
-  @tag :skip
+  # @tag :skip
   test "3" do
     assert Roman.numerals(3) == "III"
   end
 
-  @tag :skip
+  # @tag :skip
   test "4" do
     assert Roman.numerals(4) == "IV"
   end
 
-  @tag :skip
+  # @tag :skip
   test "5" do
     assert Roman.numerals(5) == "V"
   end
 
-  @tag :skip
+  # @tag :skip
   test "6" do
     assert Roman.numerals(6) == "VI"
   end
 
-  @tag :skip
+  # @tag :skip
   test "9" do
     assert Roman.numerals(9) == "IX"
   end
 
-  @tag :skip
+  # @tag :skip
   test "27" do
     assert Roman.numerals(27) == "XXVII"
   end
 
-  @tag :skip
+  # @tag :skip
   test "48" do
     assert Roman.numerals(48) == "XLVIII"
   end
 
-  @tag :skip
+  # @tag :skip
   test "59" do
     assert Roman.numerals(59) == "LIX"
   end
 
-  @tag :skip
+  # @tag :skip
   test "93" do
     assert Roman.numerals(93) == "XCIII"
   end
 
-  @tag :skip
+  # @tag :skip
   test "141" do
     assert Roman.numerals(141) == "CXLI"
   end
 
-  @tag :skip
+  # @tag :skip
   test "163" do
     assert Roman.numerals(163) == "CLXIII"
   end
 
-  @tag :skip
+  # @tag :skip
   test "402" do
     assert Roman.numerals(402) == "CDII"
   end
 
-  @tag :skip
+  # @tag :skip
   test "575" do
     assert Roman.numerals(575) == "DLXXV"
   end
 
-  @tag :skip
+  # @tag :skip
   test "911" do
     assert Roman.numerals(911) == "CMXI"
   end
 
-  @tag :skip
+  # @tag :skip
   test "1024" do
     assert Roman.numerals(1024) == "MXXIV"
   end
 
-  @tag :skip
+  # @tag :skip
   test "3000" do
     assert Roman.numerals(3000) == "MMM"
   end
