e15bac133c4fd86de7603bbf22adfa7d81f190fb
Rubén Chavarría <txingo@gmail.com>
2016-10-10 08:58:40 +0200

    Hamming on Elixir: start new exercise

---
+++ elixir/hamming/hamming.exs
@@ -0,0 +1,13 @@
+defmodule Hamming do
+  @doc """
+  Returns number of differences between two strands of DNA, known as the Hamming Distance.
+
+  ## Examples
+
+  iex> Hamming.hamming_distance('AAGTCATA', 'TAGCGATC')
+  {:ok, 4}
+  """
+  @spec hamming_distance([char], [char]) :: non_neg_integer
+  def hamming_distance(strand1, strand2) do
+  end
+end
