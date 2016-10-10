c649076388df4e439659a51f6acb397879ce1b73
Rubén Chavarría <txingo@gmail.com>
2016-10-10 08:46:52 +0200

    Roman numerals on Elixir: solve it in an algorithmic way
    There is something I don't like yet. `append_symbol` receives
    the `@symbols` element at an index and the `index` itself. There
    must be a way to eliminate that duplication.

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -1,21 +1,32 @@
 defmodule Roman do
+
+  @symbols [
+    { "M",  1000 },
+    { "CM",  900 },
+    { "D",  500 },
+    { "CD",  400 },
+    { "C",  100 },
+    { "XC",  90 },
+    { "L",  50 },
+    { "XL",  40 },
+    { "X",  10 },
+    { "IX",  9 },
+    { "V",   5 },
+    { "IV",  4 },
+    { "I",   1 }
+  ]
+
   @spec numerals(pos_integer) :: String.t
   def numerals(number) do
-    1..number
-    |> Enum.map_join(fn _ -> "I" end)
-    |> String.replace("IIIII", "V")
-    |> String.replace("VV", "X")
-    |> String.replace("VIIII", "IX")
-    |> String.replace("IIII", "IV")
-
-    |> String.replace("XXXXX", "L")
-    |> String.replace("LL", "C")
-    |> String.replace("LXXXX", "XC")
-    |> String.replace("XXXX", "XL")
-
-    |> String.replace("CCCCC", "D")
-    |> String.replace("DD", "M")
-    |> String.replace("DCCCC", "CM")
-    |> String.replace("CCCC", "CD")
+    append_symbol(number, Enum.at(@symbols, 0), 0, "")
   end
+
+  defp append_symbol(0, _, _, roman), do: roman
+  defp append_symbol(n, { roman_symbol, n_symbol }, index, roman) when n >= n_symbol do
+    append_symbol(n - n_symbol, Enum.at(@symbols, index), index, roman <> roman_symbol)
+  end
+  defp append_symbol(n, _, index, roman) do
+    append_symbol(n, Enum.at(@symbols, index + 1), index + 1, roman)
+  end
+
 end
