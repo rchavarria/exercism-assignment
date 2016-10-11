dba545f56e5a4ae7f16caeced17c0c41b2770b2c
Rubén Chavarría <txingo@gmail.com>
2016-10-11 08:58:48 +0200

    Hamming on Elixir: simplify the solution a little bit
    Reviewing other solutions, they arrange differently all `hamming`
    method's bodies, and they use less bodies than me. So,
    simplification time!

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -4,12 +4,11 @@ defmodule Hamming do
     hamming(strand1, strand2, 0)
   end
 
-  defp hamming([], [], distance), do: { :ok, distance }
-  defp hamming([], strand2, _), do: { :error, "Lists must be the same length"}
-  defp hamming(strand1, [], _), do: { :error, "Lists must be the same length"}
   defp hamming([ a | strand1 ], [ a | strand2 ], distance), do:
     hamming(strand1, strand2, distance)
   defp hamming([ a | strand1 ], [ b | strand2 ], distance), do:
     hamming(strand1, strand2, distance + 1)
+  defp hamming([], [], distance), do: { :ok, distance }
+  defp hamming(_, _, _), do: { :error, "Lists must be the same length"}
 
 end
