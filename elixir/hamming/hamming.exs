4ac357dedea6548d5ae100a942c47d113a03a286
Rubén Chavarría <txingo@gmail.com>
2016-10-11 08:51:39 +0200

    Hamming on Elixir: solve exercise
    Elixir makes this a piece of cake, because it understands
    lists and can deconstruct them, and it uses recursion for
    everything. Soooo easy.

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -1,13 +1,15 @@
 defmodule Hamming do
-  @doc """
-  Returns number of differences between two strands of DNA, known as the Hamming Distance.
-
-  ## Examples
-
-  iex> Hamming.hamming_distance('AAGTCATA', 'TAGCGATC')
-  {:ok, 4}
-  """
   @spec hamming_distance([char], [char]) :: non_neg_integer
   def hamming_distance(strand1, strand2) do
+    hamming(strand1, strand2, 0)
   end
+
+  defp hamming([], [], distance), do: { :ok, distance }
+  defp hamming([], strand2, _), do: { :error, "Lists must be the same length"}
+  defp hamming(strand1, [], _), do: { :error, "Lists must be the same length"}
+  defp hamming([ a | strand1 ], [ a | strand2 ], distance), do:
+    hamming(strand1, strand2, distance)
+  defp hamming([ a | strand1 ], [ b | strand2 ], distance), do:
+    hamming(strand1, strand2, distance + 1)
+
 end
