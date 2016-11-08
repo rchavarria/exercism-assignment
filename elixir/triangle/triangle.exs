98f5f73cee24e0609dd9ab3e66e4ff8506cbdbca
Rubén Chavarría <txingo@gmail.com>
2016-11-08 09:13:36 +0100

    Triangle on Elixir: refactor with ideas got from other users
    1. One can assign a deconstruction to a variable to use later
    
        defp classify({ :error, e } = error), do: error
    
    2. When sides are sorted, one isosceles case is redundant
    
        a, a, b
        a, b, a -> redundant
        b, a, a
    
    3. Use methods like `ok()` and `error()` as a shortcut to return
    tuples like `{ :ok, result }`
    
    There is just one idea to play with, and it's the `with-else`
    construction

--- elixir/triangle/triangle.exs
+++ elixir/triangle/triangle.exs
@@ -5,33 +5,35 @@ defmodule Triangle do
     |> Enum.sort
     |> must_be_greater_than_zero
     |> do_not_violate_inequality
-    |> is_kind(a, b, c)
+    |> is_kind
   end
 
   defp must_be_greater_than_zero(sides) do
     all_positives = Enum.all?(sides, &(&1 > 0))
     case all_positives do
-      false -> { :error, "all side lengths must be positive" }
-      true  -> { :ok, sides }
+      false -> error("all side lengths must be positive" )
+      true  -> ok(sides)
     end
   end
 
-  defp do_not_violate_inequality({ :error, e }), do: { :error, e }
+  defp do_not_violate_inequality({ :error, _ } = error), do: error
   defp do_not_violate_inequality({ :ok, sides }) do
     [x, y, z] = sides
 
     valid_sides = (z < x + y)
     case valid_sides do
-      false -> { :error, "side lengths violate triangle inequality" }
-      true  -> { :ok, sides }
+      false -> error("side lengths violate triangle inequality" )
+      true  -> ok(sides)
     end
   end
 
-  defp is_kind({ :error, e }, _, _, _), do: { :error, e }
-  defp is_kind({ :ok, _ }, a, a, a), do: { :ok, :equilateral }
-  defp is_kind({ :ok, _ }, a, a, _), do: { :ok, :isosceles }
-  defp is_kind({ :ok, _ }, a, _, a), do: { :ok, :isosceles }
-  defp is_kind({ :ok, _ }, _, a, a), do: { :ok, :isosceles }
-  defp is_kind({ :ok, _ }, _, _, _), do: { :ok, :scalene }
+  defp is_kind({ :error, _ } = error), do: error
+  defp is_kind({ :ok, [ a, a, a ] }), do: ok(:equilateral)
+  defp is_kind({ :ok, [ a, a, _ ] }), do: ok(:isosceles)
+  defp is_kind({ :ok, [ _, a, a ] }), do: ok(:isosceles)
+  defp is_kind({ :ok, _ }), do: ok(:scalene)
+
+  defp ok(payload), do: { :ok, payload }
+  defp error(message), do: { :error, message }
 
 end
