9465140af16559d0563e2e113e8f380135433038
Rubén Chavarría <txingo@gmail.com>
2016-11-30 09:13:54 +0100

    Isogram on Elixir: solved exercise

--- elixir/isogram/isogram.exs
+++ elixir/isogram/isogram.exs
@@ -4,7 +4,16 @@ defmodule Isogram do
   """
   @spec isogram?(String.t) :: boolean
   def isogram?(sentence) do
+    original_count = sentence |> sanitize |> Enum.count
+    uniq_count = sentence |> sanitize |> Enum.uniq |> Enum.count
 
+    original_count == uniq_count
+  end
+
+  defp sanitize(sentence) do
+    sentence
+    |> String.replace(~r{[ -]}, "")
+    |> String.graphemes
   end
 
 end
