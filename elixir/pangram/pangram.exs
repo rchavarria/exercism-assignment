37a78b168ba6cfed2aede9915b8d881f6ab9fd67
Rubén Chavarría <txingo@gmail.com>
2016-12-02 13:34:20 +0100

    Pangram on Elixir: extract some function to make code clearer

--- elixir/pangram/pangram.exs
+++ elixir/pangram/pangram.exs
@@ -1,12 +1,22 @@
 defmodule Pangram do
+  @invalid_graphemes ~r/[\W_0-9]/
   @minimum_alphabet_length 26
 
   def pangram?(sentence) do
     sentence
-    |> String.replace(~r/[\W_0-9]/, "")
-    |> String.graphemes
+    |> valid_graphemes
     |> Enum.uniq
-    |> Enum.count >= @minimum_alphabet_length
+    |> match_alphabet_length
+  end
+
+  defp valid_graphemes(sentence) do
+    sentence
+    |> String.replace(@invalid_graphemes, "")
+    |> String.graphemes
+  end
+
+  defp match_alphabet_length(graphemes) do
+    graphemes |> Enum.count >= @minimum_alphabet_length
   end
 
 end
