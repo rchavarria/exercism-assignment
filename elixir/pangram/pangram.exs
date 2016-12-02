3f8e5761e7ead817afc21348fce9883705c18700
Rubén Chavarría <txingo@gmail.com>
2016-12-02 13:26:42 +0100

    Pangram on Elixir: solve the exercise
    Some refactoring is pending

--- elixir/pangram/pangram.exs
+++ elixir/pangram/pangram.exs
@@ -1,18 +1,12 @@
 defmodule Pangram do
-  @doc """
-  Determines if a word or sentence is a pangram.
-  A pangram is a sentence using every letter of the alphabet at least once.
+  @minimum_alphabet_length 26
 
-  Returns a boolean.
-
-    ## Examples
-
-      iex> Pangram.pangram?("the quick brown fox jumps over the lazy dog")
-      true
-
-  """
-
-  @spec pangram?(String.t) :: boolean
   def pangram?(sentence) do
+    sentence
+    |> String.replace(~r/[\W_0-9]/, "")
+    |> String.graphemes
+    |> Enum.uniq
+    |> Enum.count >= @minimum_alphabet_length
   end
+
 end
--- elixir/pangram/pangram_test.exs
+++ elixir/pangram/pangram_test.exs
@@ -13,47 +13,47 @@ defmodule PangramTest do
     refute Pangram.pangram?("")
   end
 
-  @tag :pending
+  # @tag :pending
   test "pangram with only lower case" do
     assert Pangram.pangram?("the quick brown fox jumps over the lazy dog")
   end
 
-  @tag :pending
+  # @tag :pending
   test "missing character 'x'" do
     refute Pangram.pangram?("a quick movement of the enemy will jeopardize five gunboats")
   end
 
-  @tag :pending
+  # @tag :pending
   test "another missing character 'x'" do
     refute Pangram.pangram?("the quick brown fish jumps over the lazy dog")
   end
 
-  @tag :pending
+  # @tag :pending
   test "pangram with underscores" do
     assert Pangram.pangram?("the_quick_brown_fox_jumps_over_the_lazy_dog")
   end
 
-  @tag :pending
+  # @tag :pending
   test "pangram with numbers" do
     assert Pangram.pangram?("the 1 quick brown fox jumps over the 2 lazy dogs")
   end
 
-  @tag :pending
+  # @tag :pending
   test "missing letters replaced by numbers" do
     refute Pangram.pangram?("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog")
   end
 
-  @tag :pending
+  # @tag :pending
   test "pangram with mixed case and punctuation" do
     assert Pangram.pangram?("Five quacking Zephyrs jolt my wax bed.")
   end
 
-  @tag :pending
+  # @tag :pending
   test "pangram with non ascii characters" do
     assert Pangram.pangram?("Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.")
   end
 
-  @tag :pending
+  # @tag :pending
   test "pangram in alphabet other than ASCII" do
     refute Pangram.pangram?("Широкая электрификация южных губерний даст мощный толчок подъёму сельского хозяйства.")
   end
