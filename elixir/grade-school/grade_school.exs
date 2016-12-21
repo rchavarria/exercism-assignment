fc0dbfb9dee283f85d4346262d1a415ac0f18a27
Rubén Chavarría <txingo@gmail.com>
2016-12-21 13:42:07 +0100

    Grade school on Elixir: solved with the first solution that came to my mind

--- elixir/grade-school/grade_school.exs
+++ elixir/grade-school/grade_school.exs
@@ -1,31 +1,26 @@
 defmodule School do
-  @moduledoc """
-  Simulate students in a school.
 
-  Each student is in a grade.
-  """
-
-  @doc """
-  Add a student to a particular grade in school.
-  """
-  @spec add(map, String.t, integer) :: map
   def add(db, name, grade) do
-
+    students = Map.get(db, grade)
+    Map.put(db, grade, add_student(students, name))
   end
 
-  @doc """
-  Return the names of the students in a particular grade.
-  """
-  @spec grade(map, integer) :: [String.t]
+  defp add_student(nil, name), do: [ name ] 
+  defp add_student(students, name), do: [ name | students ]
+
   def grade(db, grade) do
-
+    Map.get(db, grade) |> not_nil
   end
 
-  @doc """
-  Sorts the school by grade and name.
-  """
-  @spec sort(map) :: [{integer, [String.t]}]
+  defp not_nil(nil), do: []
+  defp not_nil(x), do: x
+
   def sort(db) do
-
+    sorted_keys = db |> Map.keys |> Enum.sort
+    sorted_keys |> Enum.map(fn key ->
+      sorted_names = Map.get(db, key) |> Enum.sort
+      { key, sorted_names }
+    end)
   end
+
 end
--- elixir/grade-school/grade_school_test.exs
+++ elixir/grade-school/grade_school_test.exs
@@ -15,7 +15,7 @@ defmodule SchoolTest do
     assert actual == %{2 =>["Aimee"]}
   end
 
-  @tag :pending
+  # @tag :pending
   test "add more students in same class" do
     actual = @db
     |> School.add("James", 2)
@@ -25,7 +25,7 @@ defmodule SchoolTest do
     assert Enum.sort(actual[2]) == ["Blair", "James", "Paul"]
   end
 
-  @tag :pending
+  # @tag :pending
   test "add students to different grades" do
     actual = @db
     |> School.add("Chelsea", 3)
@@ -34,7 +34,7 @@ defmodule SchoolTest do
     assert actual == %{3 => ["Chelsea"], 7 => ["Logan"]}
   end
 
-  @tag :pending
+  # @tag :pending
   test "get students in a grade" do
     actual = @db
     |> School.add("Bradley", 5)
@@ -45,12 +45,12 @@ defmodule SchoolTest do
     assert Enum.sort(actual) == ["Bradley", "Franklin"]
   end
 
-  @tag :pending
+  # @tag :pending
   test "get students in a non existent grade" do
     assert [] == School.grade(@db, 1)
   end
 
-  @tag :pending
+  # @tag :pending
   test "sort school by grade and by student name" do
     actual = @db
     |> School.add("Bart", 4)
