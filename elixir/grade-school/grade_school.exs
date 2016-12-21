d4192c59feacc4fcb4ab55238c9ac3e5efe6a403
Rubén Chavarría <txingo@gmail.com>
2016-12-21 13:58:48 +0100

    Grade school on Elixir: make the solution more Elixir-ish
    - Use the pipe operator
    - Reuse functions
    - Reformat the code for better reading

--- elixir/grade-school/grade_school.exs
+++ elixir/grade-school/grade_school.exs
@@ -1,26 +1,31 @@
 defmodule School do
 
-  def add(db, name, grade) do
-    students = Map.get(db, grade)
-    Map.put(db, grade, add_student(students, name))
+  def add(db, name, student_grade) do
+    students = db |> grade(student_grade)
+                  |> add_student(name)
+    Map.put(db, student_grade, students)
   end
 
   defp add_student(nil, name), do: [ name ] 
   defp add_student(students, name), do: [ name | students ]
 
   def grade(db, grade) do
-    Map.get(db, grade) |> not_nil
+    db |> Map.get(grade)
+       |> not_nil
   end
 
   defp not_nil(nil), do: []
   defp not_nil(x), do: x
 
   def sort(db) do
-    sorted_keys = db |> Map.keys |> Enum.sort
-    sorted_keys |> Enum.map(fn key ->
-      sorted_names = Map.get(db, key) |> Enum.sort
-      { key, sorted_names }
-    end)
+    db |> sort_keys
+       |> Enum.map(fn key ->
+         { key, sort_names(db, key) }
+       end)
   end
+
+  defp sort_keys(db), do: db |> Map.keys |> Enum.sort
+
+  defp sort_names(db, key), do: db |> Map.get(key) |> Enum.sort
 
 end
