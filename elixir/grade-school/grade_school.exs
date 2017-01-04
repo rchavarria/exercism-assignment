2e40f4234b89186403043adbd8074ce833f8a554
Rubén Chavarría <txingo@gmail.com>
2017-01-04 09:01:44 +0100

    Grade school on Elixir: improve solution because of reviews
    3 main learning:
    
    1. `Map.get` accepts a third parameter, a value that will be returned if no value is found for the `key` used
    2. [`Map.update/4`](http://elixir-lang.org/docs/stable/elixir/Map.html#update/4)
    3. `Enum.map` can be used with maps too, there is no need to extract keys before with `Enum.keys |> Enum.map ...`

--- elixir/grade-school/grade_school.exs
+++ elixir/grade-school/grade_school.exs
@@ -1,31 +1,16 @@
 defmodule School do
 
-  def add(db, name, student_grade) do
-    students = db |> grade(student_grade)
-                  |> add_student(name)
-    Map.put(db, student_grade, students)
+  def add(db, name, grade) do
+    Map.update(db, grade, [ name ], &( [ name | &1 ] ))
   end
-
-  defp add_student(nil, name), do: [ name ] 
-  defp add_student(students, name), do: [ name | students ]
 
   def grade(db, grade) do
-    db |> Map.get(grade)
-       |> not_nil
+    db |> Map.get(grade, [])
+       |> Enum.sort
   end
-
-  defp not_nil(nil), do: []
-  defp not_nil(x), do: x
 
   def sort(db) do
-    db |> sort_keys
-       |> Enum.map(fn key ->
-         { key, sort_names(db, key) }
-       end)
+    Enum.map(db, fn { key, _ } -> { key, grade(db, key) } end)
   end
-
-  defp sort_keys(db), do: db |> Map.keys |> Enum.sort
-
-  defp sort_names(db, key), do: db |> Map.get(key) |> Enum.sort
 
 end
