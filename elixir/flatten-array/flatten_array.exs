ccc536ac1120e4f4b368d345d1fe91bfaf5d8be6
Rubén Chavarría <txingo@gmail.com>
2017-01-18 09:07:01 +0100

    Flatten array on Elixir: give another try to the exercise
    Instead of using the concatenation operator (`++`), it uses recursion to flatten the array.

--- elixir/flatten-array/flatten_array.exs
+++ elixir/flatten-array/flatten_array.exs
@@ -1,12 +1,12 @@
 defmodule FlattenArray do
 
   def flatten(list) do
-    list |> _flatten |> skip_nil
+    list |> _flatten([]) |> skip_nil
   end
 
-  defp _flatten([]), do: []
-  defp _flatten([ head | tail ]), do: _flatten(head) ++ _flatten(tail)
-  defp _flatten(a), do: [a]
+  defp _flatten([], acc), do: acc
+  defp _flatten([ head | tail ], acc), do: _flatten(head, _flatten(tail, acc))
+  defp _flatten(list, acc), do: [ list | acc ]
 
   defp skip_nil([]), do: []
   defp skip_nil([ nil | tail ]), do: skip_nil(tail)
