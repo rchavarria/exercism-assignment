d852c7c8149c909629fdb7f884d9fb5e6ad05305
Rubén Chavarría <txingo@gmail.com>
2017-01-19 10:19:09 +0100

    Leap on Elixir: solve the exercise with a different approach
    The previous oslution was nice, but I'd like to try another approach.
    In this one, the leap year is divided always by the three posibilities,
    creating a matrix. Then, with pattern matching, the right return value
    is selected.
    
    It's a bit obscure and cryptic, but it's different, and I like it because
    of it.

--- elixir/leap/leap.exs
+++ elixir/leap/leap.exs
@@ -1,8 +1,18 @@
 defmodule Year do
 
-  def leap_year?(year) when rem(year, 400) === 0, do: true
-  def leap_year?(year) when rem(year, 100) === 0, do: false
-  def leap_year?(year) when rem(year, 4) === 0, do: true
-  def leap_year?(_), do: false
+  @cadences [ 400, 100, 4 ]
+
+  def leap_year?(year) do
+    case leap_cadence_matrix(year) do
+      [ 0, _, _ ] -> true
+      [ _, 0, _ ] -> false
+      [ _, _, 0 ] -> true
+      _           -> false
+    end
+  end
+
+  defp leap_cadence_matrix(year) do
+    @cadences |> Enum.map(&(rem(year, &1)))
+  end
 
 end
