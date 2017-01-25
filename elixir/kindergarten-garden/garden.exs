ef31d30aaec8fdd23d14d2cfdfdbd2983ddd76d4
Rubén Chavarría <txingo@gmail.com>
2017-01-25 15:56:22 +0100

    Kindergarten garden on Elixir: refactor solution to first case

--- elixir/kindergarten-garden/garden.exs
+++ elixir/kindergarten-garden/garden.exs
@@ -8,21 +8,20 @@ defmodule Garden do
   @spec info(String.t(), list) :: map
   def info(info_string, student_names \\ nil) do
     rows = String.split(info_string, "\n")
-    IO.puts "Rows: #{inspect(rows)}"
-
-    result = Enum.map(rows, fn(row) -> 
-              IO.puts "Each row is #{inspect(row)}"
-              result = String.graphemes(row)
-                       |> Enum.map(fn plant -> @plants[plant] end)
-              IO.puts "partial result: #{inspect(result)}"
-              result
-            end)
+    result = rows
+            |> Enum.map(fn row -> String.graphemes(row) end)
             |> List.flatten
+            |> decode_plants
             |> List.to_tuple
 
     IO.puts "Result #{inspect(result)}"
 
     %{ :alice => result }
+  end
+
+  defp decode_plants(plant_codes) do
+    plant_codes
+    |> Enum.map(fn plant -> @plants[plant] end)
   end
 
 end
