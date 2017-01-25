7fff44e585180269f6b396d0ba4a89ec3be90959
Rubén Chavarría <txingo@gmail.com>
2017-01-25 13:54:29 +0100

    Kindergarten garden on Elixir: first case solved

--- elixir/kindergarten-garden/garden.exs
+++ elixir/kindergarten-garden/garden.exs
@@ -1,14 +1,28 @@
 defmodule Garden do
-  @doc """
-    Accepts a string representing the arrangement of cups on a windowsill and a
-    list with names of students in the class. The student names list does not
-    have to be in alphabetical order.
-
-    It decodes that string into the various gardens for each student and returns
-    that information in a map.
-  """
+  @plants %{
+    "R" => :radishes,
+    "C" => :clover,
+    "G" => :grass
+  }
 
   @spec info(String.t(), list) :: map
-  def info(info_string, student_names) do
+  def info(info_string, student_names \\ nil) do
+    rows = String.split(info_string, "\n")
+    IO.puts "Rows: #{inspect(rows)}"
+
+    result = Enum.map(rows, fn(row) -> 
+              IO.puts "Each row is #{inspect(row)}"
+              result = String.graphemes(row)
+                       |> Enum.map(fn plant -> @plants[plant] end)
+              IO.puts "partial result: #{inspect(result)}"
+              result
+            end)
+            |> List.flatten
+            |> List.to_tuple
+
+    IO.puts "Result #{inspect(result)}"
+
+    %{ :alice => result }
   end
+
 end
