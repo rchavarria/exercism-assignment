be81abbfde17fbe821303cd1a653b462f005c55f
Rubén Chavarría <txingo@gmail.com>
2017-09-12 17:18:24 +0200

    Rotational cipher on Elixir, refactor after code reviewing other solutions
    1. Use *character comprehension* to iterate through characters
    2. Use `rem` instead of `Integer.mod`
    3. Use ranges instead `<=` and `>=`

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -1,26 +1,24 @@
 defmodule RotationalCipher do
 
+  @lowercase ?a..?z
+  @uppercase ?A..?Z
   @alphabet_length 26
 
-  def rotate(text, 0), do: text
-  def rotate(text, @alphabet_length), do: text
   def rotate(text, shift) do
-    text
-    |> String.to_charlist()
-    |> Enum.map(fn char -> rotate_char(char, shift) end)
-    |> IO.chardata_to_string()
+    (for << char <- text >>, do: rotate_char(char, shift))
+    |> to_string()
   end
 
-  defp rotate_char(char, shift) when char >= ?a and char <= ?z do
+  defp rotate_char(char, shift) when char in @lowercase do
     rotate_anycase(char, shift, ?a)
   end
-  defp rotate_char(char, shift) when char >= ?A and char <= ?Z do
+  defp rotate_char(char, shift) when char in @uppercase do
     rotate_anycase(char, shift, ?A)
   end
   defp rotate_char(char, _shift), do: char
 
   defp rotate_anycase(char, shift, base) do
-    Integer.mod(char + shift - base, @alphabet_length) + base
+    rem(char + shift - base, @alphabet_length) + base
   end
 
 end
