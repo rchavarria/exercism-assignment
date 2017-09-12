d7c5a9ee563aed894202c2ce22c30323825c8d39
Rubén Chavarría <txingo@gmail.com>
2017-09-12 16:45:30 +0200

    Rotational cipher on Elixir, solve exercise

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -1,13 +1,27 @@
 defmodule RotationalCipher do
-  @doc """
-  Given a plaintext and amount to shift by, return a rotated string.
 
-  Example:
-  iex> RotationalCipher.rotate("Attack at dawn", 13)
-  "Nggnpx ng qnja"
-  """
-  @spec rotate(text :: String.t(), shift :: integer) :: String.t()
+  @alphabet_length 26
+
+  def rotate(text, 0), do: text
+  def rotate(text, @alphabet_length), do: text
   def rotate(text, shift) do
+    text
+    |> String.to_charlist()
+    |> Enum.map(fn char -> rotate_char(char, shift) end)
+    |> IO.chardata_to_string()
   end
+
+  defp rotate_char(char, shift) when char >= ?a and char <= ?z do
+    rotate_anycase(char, shift, ?a)
+  end
+  defp rotate_char(char, shift) when char >= ?A and char <= ?Z do
+    rotate_anycase(char, shift, ?A)
+  end
+  defp rotate_char(char, _shift), do: char
+
+  defp rotate_anycase(char, shift, base) do
+    Integer.mod(char + shift - base, @alphabet_length) + base
+  end
+
 end
 
