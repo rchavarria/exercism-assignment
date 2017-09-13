b4c6d472fb121b1ba3271a74ee20e32f6914d71b
Rubén Chavarría <txingo@gmail.com>
2017-09-13 12:26:57 +0200

    Secret handshake on Elixir, solve exercise

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -1,20 +1,42 @@
 defmodule SecretHandshake do
-  @doc """
-  Determine the actions of a secret handshake based on the binary
-  representation of the given `code`.
+  use Bitwise
 
-  If the following bits are set, include the corresponding action in your list
-  of commands, in order from lowest to highest.
+  @reverse_operation_code 0b00010000
+  @operations [
+    { 0b0001, "wink" },
+    { 0b0010, "double blink" },
+    { 0b0100, "close your eyes" },
+    { 0b1000, "jump" }
+  ]
 
-  1 = wink
-  10 = double blink
-  100 = close your eyes
-  1000 = jump
-
-  10000 = Reverse the order of the operations in the secret handshake
-  """
-  @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    @operations
+    |> decode(code, [])
+    |> remove_empty()
+    |> reverse(code)
   end
+
+  defp decode([], _code, decoded_operations), do: decoded_operations
+  defp decode([ {mask, operation} | rest_operations ], code, decoded_operations) do
+    [
+      apply_mask(code &&& mask, operation)
+      | decode(rest_operations, code, decoded_operations)
+    ]
+  end
+
+  defp apply_mask(0, _), do: ""
+  defp apply_mask(_, operation), do: operation
+
+  defp remove_empty(operations) do
+    operations |> Enum.filter(&(&1 != ""))
+  end
+
+  defp reverse(operations, code) do
+    operations |> might_reverse(code &&& @reverse_operation_code)
+  end
+
+  defp might_reverse(operations, 0), do: operations
+  defp might_reverse(operations, _do_reverse), do: operations |> Enum.reverse()
+
 end
 
