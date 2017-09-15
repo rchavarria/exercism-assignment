c87a77df2a61580cad2b0463bb86c268c66630f7
Rubén Chavarría <txingo@gmail.com>
2017-09-15 11:41:51 +0200

    Strain on Elixir, solve exercise

--- elixir/strain/strain.exs
+++ elixir/strain/strain.exs
@@ -1,21 +1,22 @@
 defmodule Strain do
-  @doc """
-  Given a `list` of items and a function `fun`, return the list of items where
-  `fun` returns true.
-
-  Do not use `Enum.filter`.
-  """
-  @spec keep(list :: list(any), fun :: ((any) -> boolean)) :: list(any)
   def keep(list, fun) do
+    filter(list, fun)
   end
 
-  @doc """
-  Given a `list` of items and a function `fun`, return the list of items where
-  `fun` returns false.
-
-  Do not use `Enum.reject`.
-  """
-  @spec discard(list :: list(any), fun :: ((any) -> boolean)) :: list(any)
   def discard(list, fun) do
+    filter(list, &(not fun.(&1)))
   end
+
+  defp filter([], _fun), do: []
+  defp filter(list = [ h | _ ], func) do
+    _filter(list, func, func.(h))
+  end
+
+  defp _filter([ _head | tail ], fun, false) do
+    filter(tail, fun)
+  end
+  defp _filter([ head | tail ], fun, true) do
+    [ head | filter(tail, fun) ]
+  end
+
 end
