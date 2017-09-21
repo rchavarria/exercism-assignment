1f359f1116b3cd36f6f1c2378086a13a9649144d
Rubén Chavarría <txingo@gmail.com>
2017-09-21 17:37:07 +0200

    Pig latin on Elixir, almost solved
    There are still a couple of corner cases that make the solution quite difficult:
    
      - `queen` -> `eenquay`
      - `square` -> `aresquay`

--- elixir/pig-latin/pig_latin.exs
+++ elixir/pig-latin/pig_latin.exs
@@ -1,20 +1,33 @@
 defmodule PigLatin do
-  @doc """
-  Given a `phrase`, translate it a word at a time to Pig Latin.
 
-  Words beginning with consonants should have the consonant moved to the end of
-  the word, followed by "ay".
+  @allowed_starts ~w{a e i o u yt xr}
 
-  Words beginning with vowels (aeiou) should have "ay" added to the end of the
-  word.
-
-  Some groups of letters are treated like consonants, including "ch", "qu",
-  "squ", "th", "thr", and "sch".
-
-  Some groups are treated like vowels, including "yt" and "xr".
-  """
-  @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
+    prefix = phrase
+             |> String.codepoints()
+             |> until_valid_start([])
+    suffix = String.slice(phrase, String.length(prefix), String.length(phrase))
+
+    # prefix <> suffix <> "ay"
+    suffix <> prefix <> "ay"
   end
+
+  defp until_valid_start(phrase, prefix) do
+    valid_start = @allowed_starts |> Enum.any?(fn start ->
+      phrase |> Enum.join() |> String.starts_with?(start)
+    end)
+
+    continue_until_valid(phrase, prefix, valid_start)
+  end
+
+  defp continue_until_valid(_phrase, prefix, true) do
+    prefix
+    |> Enum.reverse()
+    |> Enum.join()
+  end
+  defp continue_until_valid(phrase = [ first | tail ], prefix, false) do
+    until_valid_start(tail, [ first | prefix ])
+  end
+
 end
 
