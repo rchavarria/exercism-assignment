74cdfa91c4521a2495ec85468d50f486ef40ba9a
Rubén Chavarría <txingo@gmail.com>
2017-10-17 16:35:48 +0200

    Twelve days on Elixir, solve exercise (again)
    I don't know why, the implementation was empty :(

--- elixir/twelve-days/twelve_days.exs
+++ elixir/twelve-days/twelve_days.exs
@@ -1,25 +1,56 @@
 defmodule TwelveDays do
-  @doc """
-  Given a `number`, return the song's verse for that specific day, including
-  all gifts for previous days in the same line.
-  """
-  @spec verse(number :: integer) :: String.t()
+  @ordinals %{
+    1 => "first",
+      2 => "second",
+      3 => "third",
+      4 => "fourth",
+      5 => "fifth",
+      6 => "sixth",
+      7 => "seventh",
+      8 => "eighth",
+      9 => "ninth",
+      10 => "tenth",
+      11 => "eleventh",
+      12 => "twelfth",
+  }
+
+  @gifts %{
+    2 => "two Turtle Doves",
+      3 => "three French Hens",
+      4 => "four Calling Birds",
+      5 => "five Gold Rings",
+      6 => "six Geese-a-Laying",
+      7 => "seven Swans-a-Swimming",
+      8 => "eight Maids-a-Milking",
+      9 => "nine Ladies Dancing",
+      10 => "ten Lords-a-Leaping",
+      11 => "eleven Pipers Piping",
+      12 => "twelve Drummers Drumming",
+  }
+
   def verse(number) do
+    [
+      "On the #{ordinal(number)} day of Christmas my true love gave to me",
+      "#{additional_gifts(number)}#{gifts_connection(number)}a Partridge in a Pear Tree."
+    ]
+    |> Enum.join(", ")
   end
 
-  @doc """
-  Given a `starting_verse` and an `ending_verse`, return the verses for each
-  included day, one per line.
-  """
-  @spec verses(starting_verse :: integer, ending_verse :: integer) :: String.t()
+  defp ordinal(n), do: @ordinals[n]
+
+  defp additional_gifts(n), do: gifts(n) |> Enum.join(", ")
+
+  defp gifts(1), do: []
+  defp gifts(n), do: [ @gifts[n] | gifts(n - 1) ]
+
+  defp gifts_connection(1), do: ""
+  defp gifts_connection(_), do: ", and "
+
   def verses(starting_verse, ending_verse) do
+    starting_verse..ending_verse
+    |> Enum.map(&verse/1)
+    |> Enum.join("\n")
   end
 
-  @doc """
-  Sing all 12 verses, in order, one verse per line.
-  """
-  @spec sing():: String.t()
-  def sing do
-  end
+  def sing, do: verses(1, 12)
 end
-
