3a5dc68438ecfb60562ed31fa334731259537f31
Rubén Chavarría <txingo@gmail.com>
2017-10-17 16:38:07 +0200

    Twelve days on Elixir, ordinals as words

--- elixir/twelve-days/twelve_days.exs
+++ elixir/twelve-days/twelve_days.exs
@@ -1,18 +1,21 @@
 defmodule TwelveDays do
-  @ordinals %{
-    1 => "first",
-      2 => "second",
-      3 => "third",
-      4 => "fourth",
-      5 => "fifth",
-      6 => "sixth",
-      7 => "seventh",
-      8 => "eighth",
-      9 => "ninth",
-      10 => "tenth",
-      11 => "eleventh",
-      12 => "twelfth",
-  }
+
+  @ordinals ~w(
+    zeroth
+    first
+    second
+    third
+    fourth
+    fifth
+    sixth
+    seventh
+    eighth
+    ninth
+    tenth
+    eleventh
+    twelfth
+  )
+
 
   @gifts %{
     2 => "two Turtle Doves",
@@ -36,7 +39,7 @@ defmodule TwelveDays do
     |> Enum.join(", ")
   end
 
-  defp ordinal(n), do: @ordinals[n]
+  defp ordinal(n), do: @ordinals |> Enum.at(n)
 
   defp additional_gifts(n), do: gifts(n) |> Enum.join(", ")
 
