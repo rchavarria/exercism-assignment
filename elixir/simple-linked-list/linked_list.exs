39b8d2dabe773a1ba10c5c6de7519b3db89201e5
Rubén Chavarría <txingo@gmail.com>
2017-10-31 16:55:13 +0100

    Simple linked list on Elixir, add constant for empty lis
    It can be used even in pattern matching in method signatures, awesome

--- elixir/simple-linked-list/linked_list.exs
+++ elixir/simple-linked-list/linked_list.exs
@@ -1,54 +1,55 @@
 defmodule LinkedList do
   @opaque t :: tuple()
 
+  @empty_list {}
   @empty_list_error { :error, :empty_list }
 
   def new() do
-    {}
+    @empty_list
   end
 
   def push(list, elem) do
     { elem, list }
   end
 
-  def length({}), do: 0
+  def length(@empty_list), do: 0
   def length({ _elem, tail }) do
     1 + LinkedList.length(tail)
   end
 
-  def empty?({}), do: true
+  def empty?(@empty_list), do: true
   def empty?(_list), do: false
 
-  def peek({}), do: @empty_list_error
+  def peek(@empty_list), do: @empty_list_error
   def peek({ elem, _tail }) do
     { :ok, elem }
   end
 
-  def tail({}), do: @empty_list_error
+  def tail(@empty_list), do: @empty_list_error
   def tail({ _elem, next }) do
     { :ok, next }
   end
 
-  def pop({}), do: @empty_list_error
+  def pop(@empty_list), do: @empty_list_error
   def pop({ element, tail }) do
     { :ok, element, tail }
   end
 
-  def from_list([]), do: {}
+  def from_list([]), do: @empty_list
   def from_list([ head | tail ]) do
     { head, from_list(tail) }
   end
 
-  def to_list({}), do: []
+  def to_list(@empty_list), do: []
   def to_list({ elem, tail }) do
     [ elem | to_list(tail) ]
   end
 
   def reverse(list) do
-    do_reverse(list, {})
+    do_reverse(list, @empty_list)
   end
 
-  defp do_reverse({}, reversed), do: reversed
+  defp do_reverse(@empty_list, reversed), do: reversed
   defp do_reverse({ elem, tail }, reversed) do
     do_reverse(tail, reversed |> LinkedList.push(elem))
   end
